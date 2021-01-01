53bd31261c259dbc57a172dbf142a37c7268fbf3
Rafal Ostrowidzki <r.ostrowidzki@gmail.com>
2021-01-01 17:15:34 +0100

    add mailchimp module dependency

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express'),
       app = express(),
       port = 3000,
-      got = require('got');
+      got = require('got'),
+      mailchimp = require('@mailchimp/mailchimp_marketing'),
+      config = require(`${__dirname}/config.js`)
 // support parsing post request
 app.use(express.urlencoded({extended: true}));
 // support filepaths for html's src and href
-app.use(express.static(`${__dirname}`));
+app.use(express.static(`${__dirname}/public`));
+// set config for mailchimp api
+mailchimp.setConfig({
+  apiKey: config.API_KEY,
+  server: 'http://localhost:3000'
+});
 
 
 app.get("/", (req, res) => {
@@ -14,8 +21,20 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
 
-})
+  // curl -X POST \
+  //   'https://server.api.mailchimp.com/3.0/lists/{list_id}?skip_merge_validation=<SOME_BOOLEAN_VALUE>&skip_duplicate_check=<SOME_BOOLEAN_VALUE>' \
+  //   -H 'authorization: Basic <USERNAME:PASSWORD>' \
+  //   -d '{"members":[],"update_existing":false}'
 
+  // const run = async () => {
+  //   const response = await client.lists.batchListMembers("list_id", {
+  //     members: [{}],
+  //   });
+  //   console.log(response);
+  // };
+  //
+  // run();
+}
 
 
 app.listen(port, () => {
